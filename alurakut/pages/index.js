409f7c14e17102e52cf785f07e119b2b11881814
Erick Medrado <j.erick.primo@gmail.com>
2021-07-19 01:08:45 -0300

    Primeira Aula Imersão React

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,10 +1,39 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu } from '../src/lib/AlurakutCommons';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(prop) {
+    return (
+        <Box>
+            <img src={ `https://github.com/${prop.githubUser}.png` } style={{ borderRadius: '8px' }}/>
+        </Box>
+    )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+    const userName = 'erickmedrado';
+    return (
+        <>
+            <AlurakutMenu />
+            <MainGrid>
+                <div clasName="profileArea" style={{ gridArea: 'profileArea'}}>
+                    <ProfileSidebar githubUser={userName} />
+                </div>
+                <div className="welcomeArea" style={{ gridArea: 'welcomeArea'}}>
+                    <Box >
+                        Bem Vindo
+                    </Box>
+                </div>
+                <div className="relationsArea" style={{ gridArea: 'relationsArea'}}>
+                    <Box>
+                        Pessoas da Comunidade
+                    </Box>
+                    <Box>
+                        Comunidade
+                    </Box>
+                </div>
+            </MainGrid>
+        </>
+    )
 }
